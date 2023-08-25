dfe3273615c6fa8fa577757ff257d166591cc8af
Antelope10 <yejun.yun2006@gmail.com>
2023-08-25 14:53:02 -0500

    Rearranged global and local vars to optimize

--- multiple_lasso.py
+++ multiple_lasso.py
@@ -17,4 +17,4 @@ l1_penalty = [0.05, 0.1, 0.3, 0.6, 0.8] # coefficient of penalty of weights
 directory = 'Models/lasso_firstsim/'
 
 for i in range(5):
-    lasso.main(l1_penalty[i])
+    lasso.main(l1_penalty[i], i)
