1f8e4059a45d9191587c39cba8b3d40db2f3fcf5
yejun <yejun.yun2006@gmail.com>
2023-08-30 18:14:31 -0500

    Now saves csvs of snp effect sizes, mafs; and various aspects of phenotype

--- generate_phenotypes.py
+++ generate_phenotypes.py
@@ -1,47 +1,73 @@
 import torch
 import pandas as pd
+import numpy as np
 from pandas_plink import read_plink
+import math
+
+datadir = 'data/ALL_1000G_phase1integrated_v3_impute/genotypes_genome_hapgen.controls'
+outdir = 'Models/10k_maf_filtered'
+
+maf_causal_upper = 0.2
+maf_causal_lower = 0.05
+
+torch.manual_seed(0)
 
 def CSEM(phenotype): # one-dimensional tensor
-    return abs(0.5 * phenotype)
+    return abs(1 * phenotype)
 
-def r_correlation(tensor1, tensor2):
-    if tensor1.shape != tensor2.shape:
-        raise ValueError("Tensors must have the same shape")
-    tensor1_mean = torch.mean(tensor1)
-    tensor2_mean = torch.mean(tensor2)
-    tensor1_centered = tensor1 - tensor1_mean
-    tensor2_centered = tensor2 - tensor2_mean
-    correlation = torch.sum(tensor1_centered * tensor2_centered) / (torch.sqrt(torch.sum(tensor1_centered ** 2)) * torch.sqrt(torch.sum(tensor2_centered ** 2)))
-    return correlation.item()
-
-(bim, fam, bed) = read_plink('data/ALL_1000G_phase1integrated_v3_impute/genotypes_genome_hapgen.controls')
+# read genotype data
+(bim, fam, bed) = read_plink(datadir)
 genotype_df = bed.compute().T
 genotype_tensor = 2-torch.tensor(genotype_df) # read_plink encodes 0/0 homozygous reference as 2, subtraction converts to # of alternate alleles
 
+# choose 10k causal SNPs with maf within specified bounds
+maf_values = torch.sum(genotype_tensor,dim=0) / (2*genotype_tensor.shape[0])
+maf_filter = ((maf_values >= maf_causal_lower) & (maf_values <= maf_causal_upper)).float()
+indices_one = torch.nonzero(maf_filter == 1).view(-1)
+random_indices = torch.randperm(indices_one.nelement())[:10000]# Randomly choose 10k values
+maf_filter = torch.zeros_like(maf_filter) # Create a new tensor with all elements set to zero
+maf_filter[indices_one[random_indices]] = 1 # Set the chosen indices to one
+
+# data characteristics
 n_individuals = genotype_tensor.shape[0]
 n_SNPs = genotype_tensor.shape[1]
 
-SNP_effect_sizes = torch.randn(n_SNPs)
+# simulates genetic_component tensor of size n_individuals with SD = 1, 
+SNP_effect_sizes = torch.randn(n_SNPs) * maf_filter
+genetic_component = torch.mv(genotype_tensor, SNP_effect_sizes) 
+genetic_sd = torch.std(genetic_component)
+genetic_component = genetic_component / genetic_sd
 
-genetic_component = torch.mv(genotype_tensor, SNP_effect_sizes)
+# random environmental noise with SD = 1
 environmental_noise = torch.randn(n_individuals)
+
+# true phenotype
 true_phenotypes = genetic_component + environmental_noise
 
 # Define measurement error for each individual based on CSEM
 # Assuming CSEM is a function that takes true phenotypes as input and returns a tensor of standard errors
-measurement_errors = CSEM(true_phenotypes)
+measurement_errors = CSEM(true_phenotypes) * torch.randn(n_individuals)
 
 # Add measurement error to true phenotypes to get observed phenotypes
-observed_phenotypes = true_phenotypes + torch.randn(n_individuals)*measurement_errors
+observed_phenotypes = true_phenotypes + measurement_errors
 
 # Combine everything into a dataframe
 df = pd.DataFrame({
     'genetic_component': genetic_component.numpy(),
     'environmental_noise': environmental_noise.numpy(),
     'true_phenotype': true_phenotypes.numpy(),
+    'measurement_noise': measurement_errors.numpy(),
     'observed_phenotype': observed_phenotypes.numpy(),
+    
+})
+
+df_maf = pd.DataFrame({
+    'maf_values': maf_values.numpy(),
+    'maf_filter': maf_filter.numpy(),
+    'SNP_effect_sizes': SNP_effect_sizes.numpy()
 })
 
 # Export to CSV
-df.to_csv('simulated_phenotypes.csv', index=False)
+df.to_csv(outdir + '/simulated_phenotypes.csv', index=False)
+
+df_maf.to_csv(outdir + '/maf.csv', index=False)
