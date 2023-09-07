e691df0335cd3311c489f734151f817d93d2b819
Yejun Yun <yejun.yun2006@gmail.com>
2023-09-07 17:08:42 -0500

    Delete generate_phenotype.py

--- generate_phenotype.py
+++
@@ -1,8 +0,0 @@
-import torch
-from pandas_plink import read_plink
-
-(bim, fam, bed) = read_plink('data/ALL_1000G_phase1integrated_v3_impute/genotypes_genome_hapgen.controls')
-genotype_df = bed.compute().T
-genotype_tensor = 2-torch.tensor(genotype_df) # read_plink encodes 0/0 homozygous reference as 2, subtraction converts to # of alternate alleles
-
-print(genotype_tensor.dtype)
